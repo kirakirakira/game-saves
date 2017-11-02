2c6b757625d241919f6e5c605b1e9e8cd1e24064
Kira Hartlage <kira.hartlage@gmail.com>
2017-11-02 18:39:57 -0400

    changed buttons to bootstrap; changed behavior of play again and purchase next

--- tracker/templatetags/tracker_extras.py
+++ tracker/templatetags/tracker_extras.py
@@ -9,22 +9,26 @@ register = template.Library()
 @register.simple_tag
 def highest_rated_game():
     '''Get the highest rated game that has been added to the tracker'''
-    try:
-        game = Game.objects.order_by('-personal_rating').first()
-    except ValueError:
-        game = None
-    return game
+    game = Game.objects.order_by('-personal_rating').first()
+    if game is None:
+        return "I'll suggest a game for you to play again. Try adding a game to your tracker."
+    else:
+        return game
 
 @register.simple_tag
 def buy_next_game():
     '''Returns a random game from tracker that is not owned'''
-    try:
-        want_games = Game.objects.filter(own_status='Want')
-        random_index = random.randint(0, want_games.count() - 1)
-        game = want_games[random_index]
-    except ValueError:
-        game = 'You already own everything!'
-    return game
+    size = Game.objects.all().count()
+    if size > 0:
+        try:
+            want_games = Game.objects.filter(own_status='Want')
+            random_index = random.randint(0, want_games.count() - 1)
+            game = want_games[random_index]
+        except ValueError:
+            game = 'You already own everything!'
+        return game
+    else:
+        return "I'll suggest a game for you to play again. Try adding a game to your tracker."
 
 @register.simple_tag
 def num_games_played():
