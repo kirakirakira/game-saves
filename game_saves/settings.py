b61aefafb58a3f06ab5ec4dd1d16410f134e2678
Kira Hartlage <kira.hartlage@gmail.com>
2017-11-09 18:55:48 -0500

    added localhost to allowed hosts

--- game_saves/settings.py
+++ game_saves/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w+0fk0mmh5qd^5hezbv7=9o+93s9=gxdr-)x+zey@!yze!#4h$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'kirakirakira.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'kirakirakira.pythonanywhere.com']
 
 
 # Application definition
