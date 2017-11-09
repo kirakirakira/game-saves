9934a6b2e97beb88f39934123ef338d6571dd7f4
Kira Hartlage <kira.hartlage@gmail.com>
2017-11-09 18:43:18 -0500

    added pythonanywhere to allowed hosts

--- game_saves/settings.py
+++ game_saves/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w+0fk0mmh5qd^5hezbv7=9o+93s9=gxdr-)x+zey@!yze!#4h$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'kirakirakira.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
